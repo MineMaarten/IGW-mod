adc6b3f5f014408024337b4249fa34ba9a78244f
MineMaarten <loopycoaster_8@hotmail.com>
2015-07-24 17:40:44 +0200

    -Bugfix: Pop-up window when hovering over a block always says 'Press 'i' to open' regardless of the actual keybinding. Closes #44

--- src/igwmod/render/TooltipOverlayHandler.java
+++ src/igwmod/render/TooltipOverlayHandler.java
@@ -1,11 +1,14 @@
 package igwmod.render;
 
-import igwmod.ConfigHandler;
+import igwmod.ClientProxy;
 import igwmod.IGWMod;
 import igwmod.TickHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.ScaledResolution;
+
+import org.lwjgl.input.Keyboard;
+
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
@@ -14,12 +17,12 @@ public class TooltipOverlayHandler{
 
     @SubscribeEvent
     public void tickEnd(TickEvent.RenderTickEvent event){
-        if(event.phase == TickEvent.Phase.END && TickHandler.showTooltip() && ConfigHandler.shouldShowTooltip && FMLClientHandler.instance().getClient().inGameHasFocus && IGWMod.proxy.getPlayer().worldObj != null) {
+        if(event.phase == TickEvent.Phase.END && TickHandler.showTooltip() && /* ConfigHandler.shouldShowTooltip &&*/FMLClientHandler.instance().getClient().inGameHasFocus && IGWMod.proxy.getPlayer().worldObj != null) {
             Minecraft mc = FMLClientHandler.instance().getClient();
             ScaledResolution sr = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
             FontRenderer fontRenderer = FMLClientHandler.instance().getClient().fontRenderer;
             String objectName = TickHandler.getCurrentObjectName();
-            String moreInfo = "'i' for more info";
+            String moreInfo = "'" + Keyboard.getKeyName(ClientProxy.openInterfaceKey.getKeyCode()) + "' for more info";
             fontRenderer.drawString(objectName, sr.getScaledWidth() / 2 - fontRenderer.getStringWidth(objectName) / 2, sr.getScaledHeight() / 2 - 20, 0xFFFFFFFF);
             fontRenderer.drawString(moreInfo, sr.getScaledWidth() / 2 - fontRenderer.getStringWidth(moreInfo) / 2, sr.getScaledHeight() / 2 - 10, 0xFFFFFFFF);
         }
