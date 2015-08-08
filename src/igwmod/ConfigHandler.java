096ee4d84804214fe3e04f2ac67c1e517cd525d0
MineMaarten <loopycoaster_8@hotmail.com>
2015-08-08 23:28:24 +0200

    -Added config option to disable the auto-scaling to GUI scale AUTO when having the IGW GUI open. Closes #50

--- src/igwmod/ConfigHandler.java
+++ src/igwmod/ConfigHandler.java
@@ -8,6 +8,7 @@ public class ConfigHandler{
 
     public static boolean shouldShowTooltip;
     public static boolean debugMode;
+    public static boolean shouldAutoScale;
     private static Configuration conf;
 
     public static void init(File configFile){
@@ -15,6 +16,7 @@ public class ConfigHandler{
         conf.load();
         shouldShowTooltip = conf.get(Configuration.CATEGORY_GENERAL, "Should show tooltip", true).getBoolean(true);
         debugMode = conf.get(Configuration.CATEGORY_GENERAL, "Debug mode", false).getBoolean(false);
+        shouldAutoScale = conf.get(Configuration.CATEGORY_GENERAL, "Automatically scale to GUI scale AUTO in IGW GUI", true).getBoolean(true);
         conf.save();
     }
 
