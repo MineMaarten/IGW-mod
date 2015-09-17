18a71b8f14b871cf5d8324ff7439703ac9fd1d43
MineMaarten <loopycoaster_8@hotmail.com>
2015-09-17 20:34:59 +0200

    -Bugfix: IGW fails to look up pages. Closes #60

--- src/igwmod/lib/Paths.java
+++ src/igwmod/lib/Paths.java
@@ -1,9 +1,8 @@
 package igwmod.lib;
 
-import java.io.File;
 
 public class Paths{
     public static final String MOD_ID = "igwmod";
     public static final String MOD_ID_WITH_COLON = MOD_ID + ":";
-    public static final String WIKI_PATH = ":wiki" + File.separator;
+    public static final String WIKI_PATH = ":wiki/";
 }
